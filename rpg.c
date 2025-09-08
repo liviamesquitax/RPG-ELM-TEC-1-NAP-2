4841a6600c652b54a394c4831eae62e3b40ebf34
Lívia Vitória Aguiar Mesquita <liviaa.mesquitax@gmail.com>
2025-09-08 09:55:50 -0300

    Add files via upload

---
+++ rpg.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // tem que baixar o wingw pra add a biblio
+
+int main() {
+
+printf("Seja bem vindo ao nosso rpg \"ELM, uma aventura histórica\",\n (TEXTO DE INÍCIO)");
+
+printf("Você acabou de acordar ............ dito isso, você:\n");
+printf("1 - Levanta e vai explorar a casa\n");
+printf("2 - Fica deitado na cama\n");
+
